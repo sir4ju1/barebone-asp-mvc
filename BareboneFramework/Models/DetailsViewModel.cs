6fd70ceb3f10b9b7986be978644668c376c50f05
Sirajul Islam <rianaville@gmail.com>
2014-06-11 16:02:12 +0600

    details page basic layout done

---
+++ BareboneFramework/Models/DetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BareboneFramework.Models
+{
+    public class DetailsViewModel
+    {
+        public string Name { get; set; }
+        public string ItemInfo { get; set; }
+        public string Description { get; set; }
+        public string ImageLocation { get; set; }
+    }
+}
